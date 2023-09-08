0f1631f4610efddee5f292b122c09eb098d21979
Víctor Muñoz Ramírez <vicmunram@alum.us.es>
2023-09-08 18:31:37 +0200

    Updated build settings

--- Assets/Scripts/Narration/Flow/FlowStateMachine.cs
+++ Assets/Scripts/Narration/Flow/FlowStateMachine.cs
@@ -15,7 +15,6 @@ public class FlowStateMachine : MonoBehaviour
 
     private void Awake()
     {
-        Debug.Log("hey");
         ms_Instance = this;
 
         m_Channel.OnFlowStateRequested += SetFlowState;
